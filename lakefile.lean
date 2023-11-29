e57f3870b99cfa9d07a1a1edd92163da76ec37d3
Suryansh Shrivastava <86036271+Shraze97@users.noreply.github.com>
2023-11-29 17:53:52 +0530

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,22 +1,49 @@
 import Lake
 open Lake DSL
 
+def moreServerArgs := #[
+  "-Dpp.unicode.fun=true", -- pretty-prints `fun a ↦ b`
+  "-Dpp.proofs.withType=false",
+  "-DautoImplicit=false",
+  "-DrelaxedAutoImplicit=false"
+]
+
+-- These settings only apply during `lake build`, but not in VSCode editor.
+def moreLeanArgs := moreServerArgs
+
+-- These are additional settings which do not affect the lake hash,
+-- so they can be enabled in CI and disabled locally or vice versa.
+-- Warning: Do not put any options here that actually change the olean files,
+-- or inconsistent behavior may result
+def weakLeanArgs : Array String :=
+  if get_config? CI |>.isSome then
+    #["-DwarningAsError=true"]
+  else
+    #[]
+
+meta if get_config? doc = some "on" then -- do not download and build doc-gen4 by default
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+
+
 package «counterExamples» {
   -- add any package configuration options here
+}
+
+@[default_target]
+lean_exe «counterExamples» {
+  root := `Main
 }
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
-@[default_target]
-lean_lib «CounterExamples» {
-  -- add any library configuration options here
-}
 
-@[default_target]
+
 lean_lib CRTopology{
 
 }
+
+
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
